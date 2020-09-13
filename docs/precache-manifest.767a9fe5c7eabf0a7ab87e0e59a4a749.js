3ed9581a45ba325d857b70e71ecfa8a6aaade68c
Camilo <camfercas@gmail.com>
2020-09-13 17:49:29 -0300

    Cambio build

---
+++ docs/precache-manifest.767a9fe5c7eabf0a7ab87e0e59a4a749.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6243244bb5b218430ed4def04c98a4ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2647bd08bce1928a27e7",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "ecb915d0475429a73b5c",
+    "url": "/static/css/main.1a05455e.chunk.css"
+  },
+  {
+    "revision": "2647bd08bce1928a27e7",
+    "url": "/static/js/2.3e91e4b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3e91e4b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb915d0475429a73b5c",
+    "url": "/static/js/main.c1188e28.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
